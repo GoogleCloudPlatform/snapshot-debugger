7d63f86ad5a74259b0acdec3944bb2b49b7bf119
Jason Borg <jcborg@google.com>
2022-07-08 13:40:03 +0000

    Add tests for gcloud_cli_service module.

--- snapshot_dbg_cli/gcloud_cli_service.py
+++ snapshot_dbg_cli/gcloud_cli_service.py
@@ -59,73 +59,40 @@ The following unexpected error occured attempting to run the 'gcloud' cli comman
 {err}
 """
 
-CONFIG_GET_ACCOUNT_ERROR_MSG = """
-An error occured attempting to retrieve the currently configured account using
-gcloud command '{cmd}'. To note, this CLI relies on the gcloud command and
-requires that there is configured account. The error message from gcloud is:
+GCLOUD_COMMAND_FAILED_MSG = """
+Command '{command}' failed.
 
-{err}
+stdout: {stdout}
+stderr: {stderr}
+"""
+
+PARSE_GCLOUD_OUTPUT_AS_JSON_FAILED_MSG = """
+Failure occured parsing gcloud output as json.
+
+Command: '{command}'
+Data To Parse: '{gcloud_output}'
 """
 
 PARSE_CONFIGURED_ACCOUNT_ERROR_MSG = """
-An error occured attempting to parse the response from gcloud command '{cmd}'.
+An error occured attempting to parse the account obtained from gcloud'.
 
-The error was:
+Gcloud output: {gcloud_output}
 
-{err}
-
-The response was
-
-{response}
-
-However the expected format was expected to be:
-
-"<account>"
-"""
-
-CONFIG_GET_PROJECT_ERROR_MSG = """
-An error occured attempting to retrieve the currently configured project using
-gcloud command '{cmd}'. To note, this CLI relies on the gcloud command and
-requires that there is configured project. The error message from gcloud is:
-
-{err}
+However the expected format was expected to be: "<account>"
 """
 
 PARSE_CONFIGURED_PROJECT_ERROR_MSG = """
-An error occured attempting to parse the response from gcloud command '{cmd}'.
+An error occured attempting to parse the project ID obtained from gcloud'.
 
-The error was:
+Gcloud output: {gcloud_output}
 
-{err}
-
-The response was
-
-{response}
-
-However the expected format was expected to be:
-
-"<project_id>"
+However the expected format was expected to be: "<account>"
 """
 
-PRINT_ACCESS_TOKEN_ERROR = """
-An error occured attempting to retrieve the current account's access token using
-gcloud command '{cmd}'. To note, this CLI relies on the gcloud command and
-requires that there is an active login session. The error message from gcloud
-is:
+PARSE_ACCESS_TOKEN_ERROR_MSG = """
+An error occured attempting to parse the access token obtained from gcloud.
 
-{err}
-"""
-
-PARSE_ACCESS_TOKEN_ERROR = """
-An error occured attempting to parse the response from gcloud command '{cmd}'.
-
-The error was:
-
-{err}
-
-The response was
-
-{response}
+Gcloud output: {gcloud_output}
 
 However the expected format was expected to be:
 
@@ -134,37 +101,17 @@ However the expected format was expected to be:
 }}
 """
 
-API_ENABLED_CHECK_ERROR = """
-An error occurred attempting to check if API '{api_name}' is enabled using
-gcloud command '{cmd}'.  The error message from gcloud is:
+PARSE_SERVICES_ERROR_MSG = """
+An error occured attempting to parse the gcloud services output from gcloud.'.
 
-{err}
-"""
-
-ENABLE_API_ERROR = """
-An error occurred attempting to check if the Firebase Management API is enabled
-using gcloud command '{cmd}'.  The error message from gcloud is:
-
-{err}
-"""
-
-PARSE_SERVICES_ERROR = """
-An error occured attempting to parse the response from gcloud command '{cmd}'.
-
-The error was:
-
-{err}
-
-The response was:
-
-{response}
+Gcloud output: {gcloud_output}
 
 However the expected format was expected to be:
 
-# If the API service was enabled:
+# If the API service was not enabled:
 []
 
-# If the API service was not enabled, it should contain one entry with this
+# If the API service was enabled, it should contain one entry with this
 # subset of data:
 [
   {{
@@ -190,165 +137,111 @@ class GcloudCliService:
     self.user_output = user_output
 
   def config_get_account(self):
-    command = ["config", "get-value", "account", "--format=json"]
+    account = self.run(["config", "get-value", "account"])
 
-    result = self.run(command)
-
-    if result.returncode != 0:
+    # This is a sanity check, if the command succeeded and the json parsing
+    # passed, we should have non zero length string.
+    if not isinstance(account, str) or not account:
       self.user_output.error(
-          CONFIG_GET_ACCOUNT_ERROR_MSG.format(
-              cmd=result.args, err=result.stderr))
+          PARSE_CONFIGURED_ACCOUNT_ERROR_MSG.format(gcloud_output=account))
       raise SilentlyExitError
-
-    account = None
-
-    try:
-      account = json.loads(result.stdout)
-
-      # This is a sanity check, if the command succeeded and the json parsing
-      # passed, we should have non zero length string.
-      if not isinstance(account, str) or not account:
-        raise Exception
-
-    except Exception as err:
-      # We just do a blanket exception catch. We specified format=json in the
-      # gcloud command, so valid json should have been returned and the command
-      # should have returned it as an array. So any failure is an unexpected
-      # exception.
-      self.user_output.error(
-          PARSE_CONFIGURED_ACCOUNT_ERROR_MSG.format(
-              err=err, cmd=result.args, response=result.stdout))
-      raise SilentlyExitError from err
 
     return account
 
   def config_get_project(self):
-    command = ["config", "get-value", "project", "--format=json"]
+    project_id = self.run(["config", "get-value", "project"])
 
-    result = self.run(command)
-
-    if result.returncode != 0:
+    # This is a sanity check, if the command succeeded and the json parsing
+    # passed, we should have non zero length string.
+    if not isinstance(project_id, str) or not project_id:
       self.user_output.error(
-          CONFIG_GET_PROJECT_ERROR_MSG.format(
-              cmd=result.args, err=result.stderr))
+          PARSE_CONFIGURED_PROJECT_ERROR_MSG.format(gcloud_output=project_id))
       raise SilentlyExitError
-
-    project_id = None
-
-    try:
-      project_id = json.loads(result.stdout)
-
-      # This is a sanity check, if the command succeeded and the json parsing
-      # passed, we should have non zero length string.
-      if not isinstance(project_id, str) or not project_id:
-        raise Exception
-
-    except Exception as err:
-      # We just do a blanket exception catch. We specified format=json in the
-      # gcloud command, so valid json should have been returned and the command
-      # should have returned it as an array. So any failure is an unexpected
-      # exception.
-      self.user_output.error(
-          PARSE_CONFIGURED_PROJECT_ERROR_MSG.format(
-              err=err, cmd=result.args, response=result.stdout))
-      raise SilentlyExitError from err
 
     return project_id
 
-  def is_api_enabled(self, api_name):
-    command = [
-        "services", "list", "--enabled", "--format=json",
-        f"--filter=config.name={api_name}"
-    ]
+  def get_access_token(self):
+    gcloud_output = self.run(["auth", "print-access-token"])
 
-    result = self.run(command)
+    token = gcloud_output.get("token", None) \
+              if isinstance(gcloud_output, dict) else None
 
-    if result.returncode != 0:
+    if not token:
       self.user_output.error(
-          API_ENABLED_CHECK_ERROR.format(
-              api_name=api_name, cmd=result.args, err=result.stderr))
+          PARSE_ACCESS_TOKEN_ERROR_MSG.format(gcloud_output=gcloud_output))
       raise SilentlyExitError
 
-    is_enabled = False
+    return token
 
-    try:
-      # If there were no results, an empty array would be returned.
-      is_enabled = len(json.loads(result.stdout)) >= 1
+  def is_api_enabled(self, api_name):
+    services_response = self.run(
+        ["services", "list", "--enabled", f"--filter=config.name={api_name}"])
 
-    except Exception as err:
-      # We just do a blanket exception catch. We specified format=json in the
-      # gcloud command, so valid json should have been returned and the command
-      # should have returned it as an array. So any failure is an unexpected
-      # exception.
+    if not isinstance(services_response, list):
       self.user_output.error(
-          PARSE_SERVICES_ERROR.format(
-              err=err,
-              cmd=result.args,
-              response=result.stdout,
-              api_name=api_name))
-      raise SilentlyExitError from err
+          PARSE_SERVICES_ERROR_MSG.format(
+              gcloud_output=services_response, api_name=api_name))
+      raise SilentlyExitError
 
-    return is_enabled
+    # If there were no results, an empty array would be returned, and in this
+    # case an empty array indicates it wasn't enabled.
+    return len(services_response) >= 1
 
   def enable_api(self, api_name):
-    command = ["services", "enable", api_name]
+    self.run(["services", "enable", api_name])
 
-    result = self.run(command)
-
-    if result.returncode != 0:
-      self.user_output.error(
-          ENABLE_API_ERROR.format(
-              api_name=api_name, cmd=result.args, err=result.stderr))
-      raise SilentlyExitError
-
-  def get_access_token(self):
-    command = ["auth", "print-access-token", "--format=json"]
-    result = self.run(command)
-
-    if result.returncode != 0:
-      self.user_output.error(
-          PRINT_ACCESS_TOKEN_ERROR.format(cmd=result.args, err=result.stderr))
-      raise SilentlyExitError
-
-    access_token = None
-
-    try:
-      access_token = json.loads(result.stdout)["token"]
-
-    except Exception as err:
-      # We just do a blanket exception catch. We specified format=json in the
-      # gcloud command, so valid json and is should parse to a dict with a key
-      # of 'token' So any failure in the try is an unexpected exception.
-      self.user_output.error(
-          PARSE_ACCESS_TOKEN_ERROR.format(
-              err=err, cmd=result.args, response=result.stdout))
-      raise SilentlyExitError from err
-
-    return access_token
-
-  def run(self, command):
-    command_array = ["gcloud"] + command
+  def run(self, gcloud_arguments):
+    command_array = ["gcloud"] + gcloud_arguments + ["--format=json"]
+    result = None
 
     try:
       self.user_output.debug(f"Running command: '{' '.join(command_array)}'")
 
-      # Note, the stdout/stderr arguments are used here instead of
-      # 'capture_ouput', as it was added in Python 3.7 and this CLI
-      # is attempting to maintain compatibility with Python 3.6.
+      # Notes:
+      #   - The 'stdout/stderr' arguments are used here instead of
+      #     'capture_ouput', as it was added in Python 3.7 and this CLI is
+      #     attempting to maintain compatibility with Python 3.6.
+      #   - The 'universal_newlines' argument is used to turn on text mode for
+      #     the captured stdout/stderr data. This is used instead of 'text' to
+      #     be compatible with Python 3.6, as 'text' was added in 3.7
+      #   - check=False ensures on failure no exception is thrown, callers will
+      #     need to check the 'returncode' on the result.
       result = subprocess.run(
           command_array,
           stdout=subprocess.PIPE,
           stderr=subprocess.PIPE,
           universal_newlines=True,
           check=False)
-      self.user_output.debug("Result:", result)
-      return result
     except FileNotFoundError as err:
       self.user_output.error(GCLOUD_CMD_NOT_FOUND_ERROR_MSG)
       raise SilentlyExitError from err
     except OSError as err:
       self.user_output.error(GCLOUD_OS_ERROR_MSG.format(err=err))
       raise SilentlyExitError from err
-    except BaseException as err:
+    # SubprocessError is used as base class for most exceptions in the
+    # subprocess module. ValueError can also be raised if Popen is called with
+    # invalid arguments, though that would be a programing error and should only
+    # happen at dev time. We explicitly list SubproccessError here, and use
+    # Exception as a blanket catch all, these are all unexpected errors.
+    except (subprocess.SubprocessError, Exception) as err:
       self.user_output.error(UNEXPECTED_ERROR_MSG.format(err=err))
+      raise SilentlyExitError from err
+
+    if result.returncode != 0:
+      self.user_output.error(
+          GCLOUD_COMMAND_FAILED_MSG.format(
+              command=" ".join(command_array),
+              stdout=result.stdout,
+              stderr=result.stderr))
+      raise SilentlyExitError
+
+    self.user_output.debug("Result:", result)
+
+    try:
+      return json.loads(result.stdout)
+    except json.JSONDecodeError as err:
+      self.user_output.error(
+          PARSE_GCLOUD_OUTPUT_AS_JSON_FAILED_MSG.format(
+              command=" ".join(command_array), gcloud_output=result.stdout),
+          err)
       raise SilentlyExitError from err
