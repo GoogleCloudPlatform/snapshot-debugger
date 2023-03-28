cd78927eba4a6c3090e4ee79ebd31234ea81b8b1
James McTavish <mctavish@google.com>
2023-03-28 14:24:44 -0400

    Further improvements to debuggee picker display text

--- snapshot_dbg_extension/src/debuggeePicker.ts
+++ snapshot_dbg_extension/src/debuggeePicker.ts
@@ -1,18 +1,62 @@
 import { Database } from 'firebase-admin/database';
 import * as vscode from 'vscode';
 
+const NO_DEBUGGEES = 'No debuggees found';
+const NO_DEBUGGEES_DETAIL = 'Please check your configuration';
+
 class DebuggeeItem implements vscode.QuickPickItem {
     label: string;
     detail: string;
     kind?: vscode.QuickPickItemKind | undefined;
 
-    constructor(public debuggeeId: string, description: string, timestamp?: Date) {
-        this.label = description;
-        if (timestamp) {
-            this.detail = `${debuggeeId} | ${timestamp.toISOString()}`;
+    constructor(public debuggeeId: string, description: string, public timestamp: Date) {
+        if (this.debuggeeId === NO_DEBUGGEES) {
+            this.label = NO_DEBUGGEES;
+            this.detail = NO_DEBUGGEES_DETAIL;
         } else {
-            this.detail = debuggeeId;
+            this.label = description;
+            this.detail = `${debuggeeId} | ${duractionToLastActiveString(new Date().getTime() - timestamp.getTime())}`;
         }
+    }
+}
+
+function duractionToLastActiveString(d: number) {
+    const seconds = d / 1000;
+    const minutes = seconds / 60;
+
+    if (minutes < 60) {
+        return 'Recently active';
+    }
+
+    return `Last seen ${durationToFriendlyString(d)} ago`;
+}
+
+function durationToFriendlyString(d: number) {
+    const seconds = d / 1000;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+
+    if (seconds == 1) {
+        return 'a second';
+    } else if (seconds < 45) {
+        return `${Math.round(seconds)} seconds`;
+    } else if (seconds < 90) {
+        return 'a minute';
+    } else if (minutes < 45) {
+        return `${Math.round(minutes)} minutes`;
+    } else if (minutes < 90) {
+        return 'an hour';
+    } else if (hours < 24) {
+        return `${Math.round(hours)} hours`;
+    } else if (hours < 42) {
+        return 'a day';
+    } else if (days < 30) {
+        return `${Math.round(days)} days`;
+    } else if (days < 45) {
+        return 'a month';
+    } else {
+        return `${Math.round(days / 30)} months`;
     }
 }
 
@@ -31,8 +75,9 @@ async function fetchDebuggees(db: Database): Promise<DebuggeeItem[]> {
                     `${labels.module || 'default'} - ${labels.version}`,
                     timestamp));
         }
+        debuggees.sort((a: DebuggeeItem, b: DebuggeeItem) => b.timestamp.getTime() - a.timestamp.getTime());
     } else {
-        const noDebuggees = new DebuggeeItem("No debuggees found", "Please check your configuration");
+        const noDebuggees = new DebuggeeItem(NO_DEBUGGEES, "", new Date());
         noDebuggees.debuggeeId = "";
         debuggees.push();
     }
